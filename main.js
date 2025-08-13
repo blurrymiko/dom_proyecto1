a564b3372a489b70b1e80ac5003a3bb149b02405
blurrymiko <blurrymiko@outlook.com>
2025-08-13 15:50:59 +0200

    new:main.js

---
+++ main.js
@@ -0,0 +1,101 @@
+const products = [
+  {
+    name: 'La chica que vive al final del camino',
+    price: 13.29,
+    stars: 5,
+    reviews: 35,
+    seller: 'BookStore Madrid',
+    image:
+      'ttps://cdn.kobo.com/book-images/a46c9596-9286-4216-89cc-73ada0006385/353/569/90/False/la-chica-que-vive-al-final-del-camino.jpg'
+  },
+
+  {
+    name: 'Donde nacen las bestias',
+    price: 9.49,
+    stars: 4,
+    reviews: 12,
+    seller: 'BookStore Madrid',
+    image:
+      'https://cdn.kobo.com/book-images/3b6ac656-aaf1-4af8-bd63-3c7ce67880ab/353/569/90/False/donde-nacen-las-bestias.jpg'
+  },
+
+  {
+    name: 'Nunca mientas',
+    price: 9.49,
+    stars: 3.5,
+    reviews: 22,
+    seller: 'BookStore Madrid',
+    image:
+      'https://cdn.kobo.com/book-images/7bc75543-b3db-4855-91f3-1688daf5c213/353/569/90/False/nunca-mientas.jpg'
+  },
+
+  {
+    name: 'No tengas miedo',
+    price: 12.34,
+    stars: 5,
+    reviews: 64,
+    seller: 'BookStore Madrid',
+    image:
+      'https://cdn.kobo.com/book-images/e9958aae-542c-4ee1-ac48-1910ec665ef5/353/569/90/False/no-tengas-miedo-3.jpg'
+  },
+
+  {
+    name: 'La asistenta',
+    price: 9.49,
+    stars: 4.5,
+    reviews: 10,
+    seller: 'BookStore Madrid',
+    image:
+      'https://cdn.kobo.com/book-images/52bbf8e8-e8bd-4b9e-a0b0-005d7b6e6fae/353/569/90/False/la-asistenta-la-asistenta-1-1.jpg'
+  },
+
+  {
+    name: 'Las abandonadas',
+    price: 11.39,
+    stars: 3,
+    reviews: 8,
+    seller: 'BookStore Madrid',
+    image:
+      'https://cdn.kobo.com/book-images/c3ee6560-5458-4556-8b46-d356b4c4ea52/353/569/90/False/las-abandonadas.jpg'
+  }
+]
+
+const ereaders = [
+  {
+    name: 'Kobo Elipsa 2E',
+    price: 349.99,
+    stars: 4,
+    reviews: 35,
+    seller: 'BookStore Madrid',
+    image:
+      'https://es.kobobooks.com/cdn/shop/files/ES-ES_1-DeviceStylus_1080x1080_9acd7bcf-de51-46de-8bd3-610d77a05432_575x575.jpg'
+  },
+  {
+    name: 'Kobo Clara BW',
+    price: 149.99,
+    stars: 4,
+    reviews: 10,
+    seller: 'BookStore Madrid',
+    image:
+      'https://es.kobobooks.com/cdn/shop/files/1-Spa-BW-ES-ES-Device_Front_1080x1080_0bc13994-8628-4d73-948a-be1e0d15dd3e_575x575.png'
+  },
+
+  {
+    name: 'Kobo Sage',
+    price: 299.99,
+    stars: 5,
+    reviews: 43,
+    seller: 'BookStore Madrid',
+    image: 'https://es.kobobooks.com/cdn/shop/files/Cadmus_Dual_ES_575x575.jpg'
+  },
+
+  {
+    name: 'Kobo Libra Colour',
+    price: 219.99,
+    stars: 4.5,
+    reviews: 22,
+    seller: 'BookStore Madrid',
+    image:
+      'https://es.kobobooks.com/cdn/shop/files/2-Monza-Black-StylusAttached-ES-ES-Device_1080x1080_3dd4bb21-18f9-48fe-8042-a36e8b3e6072_431x431.png'
+  }
+]
